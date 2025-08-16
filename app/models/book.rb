be4a69b131ca70cf364ad7f737d6fb7b0d670e3c
Tiago Freire <tf.swsolutions@pm.me>
2025-08-15 23:56:05 -0300

    Add Book specs

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,14 @@
 class Book < ApplicationRecord
+  has_many :book_reservations, dependent: :destroy
+
   validates :title, presence: true
   validates :title, uniqueness: true
+
+  def available?
+    book_reservations.where(finished_at: nil).none?
+  end
+
+  def reserved?
+    book_reservations.where(finished_at: nil).any?
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,8 +1,43 @@
 require 'rails_helper'
 RSpec.describe Book, type: :model do
+  let(:created_book) { create(:book) }
+  let(:user) { create(:user) }
+
   subject { build(:book) }
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
+  end
+
+  describe "available?" do
+    subject { created_book.available? }
+
+    context "when there are no active reservations" do
+      let!(:book_reservation) { create(:book_reservation, book: created_book, finished_at: DateTime.yesterday, user: user) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when there are active reservations" do
+      let!(:book_reservation) { create(:book_reservation, book: created_book, finished_at: nil, user: user) }
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe "reserved?" do
+    subject { created_book.reserved? }
+
+    context "when there are reservations" do
+      let!(:book_reservation) { create(:book_reservation, book: created_book, finished_at: nil, user: user) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when there are no reservations" do
+      let!(:book_reservation) { create(:book_reservation, book: created_book, finished_at: DateTime.yesterday, user: user) }
+
+      it { is_expected.to be_falsey }
+    end
   end
 end
